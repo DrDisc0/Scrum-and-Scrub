dff3ea7b0c6928b2099ef4ae9775b0c167a03209
danielle217 <99216683+danielle217@users.noreply.github.com>
2024-11-06 01:46:46 -0500

    Update index.js

--- api-devlopment/index.js
+++ api-devlopment/index.js
@@ -1,14 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+//app.jS
+import express from 'express';
+import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
-const port = process.env.SERVER_PORT || 5000;
+const PORT = 5000;
 
-app.use(express.urlencoded({ extended:true }));
-app.use('/', require('./route'));
-/*app.get('/', function(req, res) {
-    res.sendFile('index.html', {root:__dirname});
-});*/
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-app.listen(port, () => {
-    console.log('Now listening on port ' + port)
+app.use('/users', usersRoutes);
+
+app.get("/", (req, res) => {
+    console.log('working');
+    res.send("Welcome to the Users API!")
 });
+app.use((err,req,res,next) => {
+    console.error(err.stack);
+    res.status(500).send('something broke');
+});
+  
+  app.listen(5000, () => {
+      console.log(`Server listening to port ${PORT}`);
+  });
